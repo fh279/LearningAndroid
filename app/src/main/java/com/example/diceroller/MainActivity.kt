ce5ff53e0e5517ac5617c365e62aa8e9fccefce5
Pogorelov <mikhail.pogorelov@myoffice.team>
2023-10-10 11:05:01 +0300

    daily commit. nothing special

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
