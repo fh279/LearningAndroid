c21f0383637b7b81ffd931a7ca30e4b1911b19a6
Pogorelov <mikhail.pogorelov@myoffice.team>
2023-10-11 21:37:49 +0300

    added listeners to buttons

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.example.diceroller
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    // @SuppressLint("MissingInflatedId", "SetTextI18n")
+    @SuppressLint("MissingInflatedId", "SetTextI18n", "CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNumOne = findViewById<Button>(R.id.btnNum1)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+        val firstInputField = findViewById<EditText>(R.id.editTextNumber1)
+        val secondInputField = findViewById<EditText>(R.id.editTextNumber2)
+        val alreadyInField = firstInputField.text.toString()
+        val textToAdd = getString(R.string.num1)
+
+        val listOfNumButtons = listOf<Button>(
+            findViewById(R.id.btnNum0),
+            findViewById(R.id.btnNum1),
+            findViewById(R.id.btnNum2),
+            findViewById(R.id.btnNum3),
+            findViewById(R.id.btnNum4),
+            findViewById(R.id.btnNum5),
+            findViewById(R.id.btnNum6),
+            findViewById(R.id.btnNum7),
+            findViewById(R.id.btnNum8),
+            findViewById(R.id.btnNum9),
+        )
+        fun listenNums() {
+            for (numButton in listOfNumButtons) {
+                numButton.setOnClickListener { firstInputField.append(alreadyInField + listOfNumButtons.indexOf(numButton)) }
+            }
+        }
+        listenNums()
+
+        fun clearTextFields() {
+            firstInputField.text.clear()
+            secondInputField.text.clear()
+        }
+
+        btnClear.setOnClickListener { clearTextFields() }
     }
 }
